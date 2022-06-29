c6d9035e80ab8f491c9e2aeb664eda07f3b2e81a
rowida22 <rowida91200@gmail.com>
2022-06-29 14:59:39 +0200

    Done

--- Test-Suite_login/test_20_login.py
+++ Test-Suite_login/test_20_login.py
@@ -22,10 +22,15 @@ class test_20_login(unittest.TestCase):
     def test_20(self):
         """ Verify that if the user clicks on the forgotten password link then 
         the user should be navigated to the forgot password page."""
-        self.forgot_password.click()  # pylint: disable=no-member
+        self.email.send_keys(  # pylint: disable=no-member
+            self.testdata.EMAIL_VALID)
+        self.password.send_keys(  # pylint: disable=no-member
+            self.testdata.PASSWORD_VALID)
+        self.login.click()
+        self.forgot_password.click()
         self.assertTrue(self.email.is_displayed(), "Email field is not displayed")
         self.assertTrue(self.password.is_displayed(), "Password field is not displayed")
-
+    
     def tearDown(self):
         """called after every test"""
         TearDown(self.driver)
